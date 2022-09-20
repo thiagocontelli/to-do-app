03bbaabeec49101100d90d46488f2c08eee91c71
Thiago Contelli <thiagocontellid@gmail.com>
2022-09-20 19:54:40 -0300

    feat: theme created and fonts imported

--- App.tsx
+++ App.tsx
@@ -1,12 +1,24 @@
 import { StatusBar } from 'react-native';
 import { Home } from './src/screens/Home/index';
-import { useFonts } from 'expo-font';
+import { ThemeProvider } from 'styled-components';
+import {
+	useFonts,
+	Inter_400Regular,
+	Inter_700Bold,
+} from '@expo-google-fonts/inter';
+import theme from './src/theme';
 
 export default function App() {
+	const [fontsLoaded] = useFonts({ Inter_400Regular, Inter_700Bold });
+
 	return (
-		<>
-			<StatusBar barStyle="light-content" />
-			<Home />
-		</>
+		<ThemeProvider theme={theme}>
+			<StatusBar
+				barStyle="light-content"
+				backgroundColor="transparent"
+				translucent
+			/>
+			{fontsLoaded && <Home />}
+		</ThemeProvider>
 	);
 }
---
+++ src/theme/index.ts
@@ -0,0 +1,30 @@
+export default {
+	COLORS: {
+		WHITE: '#FFFFFF',
+
+		PURPLE: '#8284FA',
+		PURPLE_DARK: '#5E60CE',
+
+		BLUE: '#4EA8DE',
+		BLUE_DARK: '#1E6F9F',
+
+		GRAY_700: '#121214',
+		GRAY_600: '#202024',
+		GRAY_500: '#29292E',
+		GRAY_400: '#323238',
+		GRAY_300: '#7C7C8A',
+		GRAY_200: '#C4C4CC',
+		GRAY_100: '#E1E1E6',
+
+		DANGER: '#E25858',
+	},
+	FONT_FAMILY: {
+		REGULAR: 'Roboto_400Regular',
+		BOLD: 'Roboto_700Bold',
+	},
+	FONT_SIZE: {
+		SM: 12,
+		MD: 14,
+		LG: 16,
+	},
+};
